f343fdcfbeeece80b3957d7769e57ef557f981c0
TParis <tparis@ufpfleet.org>
2024-04-23 20:02:48 +0900

    Add checklist type to keyname in project model export method

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -403,7 +403,7 @@ class Project extends Model
             if (is_array($value)) {
                 unset($export[$key]);
             } else if ($value == 0 || $value == 1) {
-                $export["checklist_" + $key] = $value;
+                $export["checklist_" . $key] = $value;
                 unset($export[$key]);
             } else {
                 $export[$key] = e($value);
